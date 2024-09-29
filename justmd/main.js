17d0f502e442ccabd562a6c8b8583d35e0194bb9
Bak Gergely János <16374863+weyh@users.noreply.github.com>
2024-09-29 16:32:14 +0200

    Fix justmd on mobile

--- justmd/main.js
+++ justmd/main.js
@@ -1,27 +1,28 @@
-const doneTypingInterval = 1000;
-let typingTimer = null;
+const STORAGE_NAME = "md";
+const DONE_TYPING_INTERVAL = 750;
 
+let typingTimer = null;
 const converter = new showdown.Converter();
 
-document.addEventListener('DOMContentLoaded', () => {
-    const textarea = document.querySelector('textarea');
-    const markdownDisplay = document.querySelector('#md');
+document.addEventListener("DOMContentLoaded", () => {
+    const textarea = document.querySelector("textarea");
+    const markdownDisplay = document.querySelector("#md");
 
     function doneTyping() {
-        localStorage.setItem("text", textarea.value);
+        localStorage.setItem(STORAGE_NAME, textarea.value);
     }
 
     textarea.addEventListener("input", _ => {
         if (typingTimer != null)
             clearTimeout(typingTimer);
 
-        typingTimer = setTimeout(doneTyping, doneTypingInterval);
+        typingTimer = setTimeout(doneTyping, DONE_TYPING_INTERVAL);
     });
 
     textarea.addEventListener("keyup", _ => {
         markdownDisplay.innerHTML = converter.makeHtml(textarea.value);
     });
 
-    textarea.value = localStorage.getItem("text");
+    textarea.value = localStorage.getItem(STORAGE_NAME);
     markdownDisplay.innerHTML = converter.makeHtml(textarea.value);
 });
